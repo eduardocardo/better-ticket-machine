f46716f582b4b1bda159f15b7793b71dc7e4327b
Eduardo <eduardocardomartinez@gmail.com>
2014-11-07 13:31:24 +0100

    añadido metodo getAmountLeftTopay que calcula lo que falta para imprimir un ticket

--- TicketMachine.java
+++ TicketMachine.java
@@ -99,4 +99,14 @@ public class TicketMachine
         balance = 0;
         return amountToRefund;
     }
+    /**Devuelve el dinero que hace falta meter
+     * para poder imprimir un ticket
+     * 
+     */
+    public int getAmountLeftToPay()   
+    {
+        int amountLeftToPay;
+        amountLeftToPay=(price-balance);
+        return amountLeftToPay;
+    }
 }
